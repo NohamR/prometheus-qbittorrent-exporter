ff9e4cfabd09f50b63c7166da10298fb618ea936
√(noham)² <noham.rivoirard@gmail.com>
2024-01-21 21:25:39 +0100

    c

--- qbittorrent_exporter/exporter.py
+++ qbittorrent_exporter/exporter.py
@@ -1,8 +1,8 @@
 import faulthandler
 import logging
 import os
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 import signal
 import sys
 import time
